6c633a11d413420cb3b3fe8682dc905086fbe2ac
ChaiGans <elbertchailes888@gmail.com>
2024-05-10 16:59:35 +0700

    feat: authentication using token jwt as cookies

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 import { validateBodyFields } from "./middleware/error_handling.js";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 import express from "express";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
 dotenv.config();
@@ -12,6 +14,7 @@ const prisma = new PrismaClient();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(cookieParser());
 
 const hashPassword = async (pw) => {
   const salt = await bcrypt.genSalt(10);
@@ -20,15 +23,30 @@ const hashPassword = async (pw) => {
 };
 
 const passwordChecking = async (pw, hashedPw) => {
-  console.log(pw);
-  console.log(hashedPw);
-  const result = await bcrypt.compare(pw, hashedPw);
-  return result;
+  return bcrypt.compare(pw, hashedPw);
 };
+
+function authenticateToken(req, res, next) {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.sendStatus(401);
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+    req.user = user;
+    next();
+  });
+}
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "300s" });
+}
 
 app.listen(3000, (err) => {
   if (err) {
-    console.log("Error listening to port " + 3000);
+    console.error("Error listening to port 3000");
   } else {
     console.log("Currently listening to port 3000");
   }
@@ -39,17 +57,16 @@ app.post(
   validateBodyFields(["username", "password", "email"]),
   async (req, res) => {
     const { username, password, email } = req.body;
-
-    // Password hashing and salting
-    const hashedPassword = await hashPassword(password);
-    const newData = { username, password: hashedPassword, email };
-
-    // Inserting data into User table
-    const createUser = await prisma.user.create({ data: newData });
-    if (createUser) {
-      return res.status(200).send("Data create success");
+    try {
+      const hashedPassword = await hashPassword(password);
+      const newUser = await prisma.user.create({
+        data: { username, password: hashedPassword, email },
+      });
+      res.status(200).send("User registration successful");
+    } catch (error) {
+      console.error("Error during registration:", error);
+      res.status(500).send("Error creating user");
     }
-    return res.status(400).send("Data failed to create");
   }
 );
 
@@ -58,29 +75,34 @@ app.post(
   validateBodyFields(["username", "password"]),
   async (req, res) => {
     const { username, password } = req.body;
-
     try {
-      // Retrieve user and their hashed password from database
-      const user = await prisma.user.findFirst({
-        where: { username: username },
-        select: { password: true },
+      const user = await prisma.user.findUnique({
+        where: { username },
       });
-
-      // Check if user was found
       if (!user) {
         return res.status(404).send("User not found");
       }
-
-      // Check if the provided password matches the stored hashed password
-      const isPasswordTrue = await passwordChecking(password, user.password);
-      if (!isPasswordTrue) {
-        return res.status(400).send("Password wrong");
+      const isValid = await passwordChecking(password, user.password);
+      if (!isValid) {
+        return res.status(400).send("Incorrect password");
       }
-
-      return res.status(200).send("Login success");
+      const accessToken = generateAccessToken({ username });
+      res.cookie("token", accessToken, {
+        httpOnly: true,
+        secure: true,
+        maxAge: 300000, // 5 menit
+      });
+      res.status(200).send("Login successful");
     } catch (error) {
-      console.log(error);
-      return res.status(500).send("An error occurred during the login process");
+      console.error("Login error:", error);
+      res.status(500).send("An error occurred during the login process");
     }
   }
 );
+
+app.get("/", authenticateToken, (req, res) => {
+  if (!req.user) {
+    res.redirect("http://127.0.0.1:5500/login.html");
+  }
+  res.status(200).send(`Hello ${req.user.username}`);
+});
