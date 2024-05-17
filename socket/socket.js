165057aa8a035a0509d43af05b5768814ee2ea73
ChaiGans <elbertchailes888@gmail.com>
2024-05-18 00:42:15 +0700

    fix: connection server to frontend

--- socket/socket.js
+++ socket/socket.js
@@ -1,23 +1,44 @@
 import { Server } from "socket.io";
-import http from "http";
-import express from "express";
 
-const app = express();
-
-const server = http.createServer(app);
-const io = new Server(server, {
+const initializeSocketIO = (server) => {
+  const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
-})
+      origin: "http://localhost:5173", // Ensure this is the correct client URL
+      methods: ["GET", "POST"],
+    },
+  });
 
-io.on("connection", (socket) => {
-    console.log("User Connected: ", socket.id);
+  const users = {};
+
+  io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    socket.on("login", ({ username }) => {
+      users[username] = socket.id;
+      socket.username = username;
+      console.log(`User ${username} logged in`);
+    });
+
+    socket.on("send_message", ({ message, toUserId }) => {
+      const recipientSocketId = users[toUserId];
+      if (recipientSocketId) {
+        io.to(recipientSocketId).emit("receive_message", {
+          message,
+          fromUserId: socket.userId,
+        });
+        console.log(`Message sent from ${socket.userId} to ${toUserId}`);
+      } else {
+        socket.emit("error", { message: "User not connected" });
+      }
+    });
 
     socket.on("disconnect", () => {
-        console.log("User Disconnected: ", socket.id);
-    })
-})
+      console.log(`User ${socket.username} disconnected`);
+      delete users[socket.username];
+    });
+  });
 
-export {app, io, server}
+  return io;
+};
+
+export default initializeSocketIO;
